7e6f2bcdc94efa9d04d6ae7219b0cde745b1275b
phuongphonui98 <phuongphonui98@gmail.com>
2021-09-10 00:12:38 +0700

    [REF] sgvn_purchase_quotation: add 2 report

--- sgvn_purchase_quotation/__manifest__.py
+++ sgvn_purchase_quotation/__manifest__.py
@@ -12,10 +12,19 @@
         "x_company_organization", "purchase_stock", "sales_team", "x_partner"
     ],
     "data": [
+        # SECURITY
         "security/ir.model.access.csv",
+        # VIEWS
         "views/company.xml",
         "views/purchase.xml",
         "views/partner.xml",
+        # REPORTS
+        "reports/action.xml",
+        "reports/layout_quotation_request_report.xml",
+        "reports/layout_estimate request_report.xml",
+        "reports/quotation_request_template_report.xml",
+        "reports/estimate_request_template_report.xml",
+
     ],
     "application": False,
     "installable": True,
