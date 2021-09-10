e492d5d7f9d308082fe4f5e5eb1dc839d4d7c6bd
Hao Quach <49672733+haoquach1996@users.noreply.github.com>
2021-09-10 10:02:43 +0700

    thêm log

--- sgvn_purchase_quotation/models/purchase.py
+++ sgvn_purchase_quotation/models/purchase.py
@@ -16,7 +16,8 @@ class PurchaseOrder(models.Model):
     def _onchange_partner_id_sgvn(self):
         self.trans_classify_id = self.partner_id.x_transaction_classification[0].id if self.partner_id.x_transaction_classification else False
         _logger.info('111111111111 self.partner_id.x_organization_id %s,  x_organization_id %s', self.partner_id.x_organization_id, self._default_organization_id())
-        if not self.partner_id.x_organization_id or self.partner_id.x_organization_id.id != self._default_organization_id():
+        if not (self.partner_id.x_organization_id and self.partner_id.x_organization_id.id == self._default_organization_id()):
+            _logger.info('22222222222222222222')
             return {
                 'name': 'Alert: Insufficient supplier transaction information',
                 'type': 'ir.actions.act_window',
