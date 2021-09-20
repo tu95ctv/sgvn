af8eb6ed8b12e391386469c4c1a65a567c22303e
Hao Quach <49672733+haoquach1996@users.noreply.github.com>
2021-09-20 16:58:00 +0700

    [update] x_purchase_order: split picking with location

--- x_purchase_order/models/__init__.py
+++ x_purchase_order/models/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import product_product
-from . import purchase
+from . import purchase_order
+from . import purchase_order_line
 from . import res_config_settings
+from . import stock_picking
