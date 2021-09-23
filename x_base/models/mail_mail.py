6f291cf642402bb815c98b929894e8015e8db5eb
Hao Quach <49672733+haoquach1996@users.noreply.github.com>
2021-09-23 16:24:15 +0700

    Update mail_mail.py

--- x_base/models/mail_mail.py
+++ x_base/models/mail_mail.py
@@ -10,12 +10,14 @@ class MailMail(models.Model):
     _inherit = 'mail.mail'
 
     def _send_prepare_values(self, partner=None):
-        if self.model == 'purchase.order':
-            po = self.env[self.model].sudo().brose(self.res_id)
-            if po.state in ['draft', 'sent']:
-                partner_email_field = 'email_quote_request'
-            else:
-                partner_email_field = 'email_purchase'
-            if not self._context.get('partner_email_field'):
-                return super(MailMail, self).with_context(partner_email_field=partner_email_field)._send_prepare_values(partner)
-        return super(MailMail, self)._send_prepare_values(partner)
+        # if self.model == 'purchase.order':
+        #     po = self.env[self.model].sudo().brose(self.res_id)
+        #     if po.state in ['draft', 'sent']:
+        #         partner_email_field = 'email_quote_request'
+        #     else:
+        #         partner_email_field = 'email_purchase'
+        #     if not self._context.get('partner_email_field'):
+        #         return super(MailMail, self).with_context(partner_email_field=partner_email_field)._send_prepare_values(partner)
+        res = super(MailMail, self)._send_prepare_values(partner)
+        _logger.info('3333333333333333333 %s', res)
+        return res
