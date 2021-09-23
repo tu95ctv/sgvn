10bb7c1d7a13f7c2acd2ccaf664b24d1d8070c01
Hao Quach <49672733+haoquach1996@users.noreply.github.com>
2021-09-23 16:16:35 +0700

    Update res_partner.py

--- x_base/models/res_partner.py
+++ x_base/models/res_partner.py
@@ -12,16 +12,17 @@ class ResPartner(models.Model):
         res_model = self._context.get('default_model', False)
         res_id = self._context.get('default_res_id', False)
         if res_model and res_id and res_model == 'purchase.order' and 'email' in list_fields:
-            po = self.env[res_model].sudo().browse(res_model)
-            if po.state in ['draft', 'sent']:
-                partner_email_field = 'email_quote_request'
-            else:
-                partner_email_field = 'email_purchase'
-            list_fields += [partner_email_field]
-            res = super(ResPartner, self).read(list_fields, load)
-            _logger.info('2222222222222222222222 %s', res)
+            if 'mail_post_autofollow' in self._context or 'force_email' in self._context or 'show_email' in self._context:
+                po = self.env[res_model].sudo().browse(res_id)
+                if po.state in ['draft', 'sent']:
+                    partner_email_field = 'email_quote_request'
+                else:
+                    partner_email_field = 'email_purchase'
+                list_fields += [partner_email_field]
+                res = super(ResPartner, self).read(list_fields, load)
+                _logger.info('2222222222222222222222 %s', res)
 
-            for r in res:
-                r['email'] = r[partner_email_field]
-            return res
+                for r in res:
+                    r['email'] = r[partner_email_field]
+                return res
         return super(ResPartner, self).read(list_fields, load)
