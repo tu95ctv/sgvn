7eb0abdbf0ac33552d1539609e19e660d83a8f00
Hao Quach <49672733+haoquach1996@users.noreply.github.com>
2021-09-23 17:44:20 +0700

    bo log

--- x_base/models/mail_mail.py
+++ x_base/models/mail_mail.py
@@ -10,41 +10,14 @@ _logger = logging.getLogger(__name__)
 class MailMail(models.Model):
     _inherit = 'mail.mail'
 
-    # def _send_prepare_values(self, partner=None):
-    #     # if self.model == 'purchase.order':
-    #     #     po = self.env[self.model].sudo().brose(self.res_id)
-    #     #     if po.state in ['draft', 'sent']:
-    #     #         partner_email_field = 'email_quote_request'
-    #     #     else:
-    #     #         partner_email_field = 'email_purchase'
-    #     #     if not self._context.get('partner_email_field'):
-    #     #         return super(MailMail, self).with_context(partner_email_field=partner_email_field)._send_prepare_values(partner)
-    #     _logger.info('22222222222222222222  _send_prepare_values%s', self._context)
-
-    #     res = super(MailMail, self)._send_prepare_values(partner)
-    #     _logger.info('44444444444444444444  _send_prepare_values %s', res)
-
-    #     return res
-
     def _send_prepare_values(self, partner=None):
-        """Return a dictionary for specific email values, depending on a
-        partner, or generic to the whole recipients given by mail.email_to.
-
-            :param Model partner: specific recipient partner
-        """
-        self.ensure_one()
-        body = self._send_prepare_body()
-        body_alternative = tools.html2plaintext(body)
-        if partner:
-            email_to = [tools.formataddr((partner.name or 'False', partner.email or 'False'))]
-            _logger.info('777777775555555555555555  _send_prepare_values %s', partner.email)
-            _logger.info('77777777777777777  _send_prepare_values %s', email_to)
-        else:
-            email_to = tools.email_split_and_format(self.email_to)
-            _logger.info('88888888888888888  _send_prepare_values %s', email_to)
-        res = {
-            'body': body,
-            'body_alternative': body_alternative,
-            'email_to': email_to,
-        }
+        res = super(MailMail, self)._send_prepare_values(partner)
+        if 'partner_email_field' in self._context:
+            if partner:
+                email = getattr(partner, self._context.get('partner_email_field'))
+                email_to = [tools.formataddr((partner.name or 'False', email))]
+            else:
+                email_to = tools.email_split_and_format(self.email_to)
+            res.update({'email_to': email_to})
+        _logger.info('88888888888888888  _send_prepare_values %s', res)
         return res
--- x_purchase_quotation/models/res_partner.py
+++ x_purchase_quotation/models/res_partner.py
@@ -13,35 +13,14 @@ class ResPartner(models.Model):
     email_quote_request = fields.Char("E-mail address for sending a quote request", copy=False)
     email_purchase = fields.Char("Purchase order sending email address", copy=False)
 
-    def read(self, list_fields=None, load='_classic_read'):
-        _logger.info('22222222222222222222 %s', self._context)
+    # def read(self, list_fields=None, load='_classic_read'):
+    #     if 'partner_email_field' in self._context:
+    #         partner_email_field = self._context.get('partner_email_field')
+    #         list_fields += [partner_email_field]
+    #         res = super(ResPartner, self).read(list_fields, load)
+    #         for r in res:
+    #             r['email'] = r[partner_email_field]
 
-        # res_model = self._context.get('default_model', False)
-        # res_id = self._context.get('default_res_id', False)
-        # if res_model and res_id and res_model == 'purchase.order' and 'email' in list_fields:
-        #     if 'mail_post_autofollow' in self._context or 'force_email' in self._context or 'show_email' in self._context:
-        #         po = self.env[res_model].sudo().browse(res_id)
-        #         if po.state in ['draft', 'sent']:
-        #             partner_email_field = 'email_quote_request'
-        #         else:
-        #             partner_email_field = 'email_purchase'
-        #         list_fields += [partner_email_field]
-        #         res = super(ResPartner, self).read(list_fields, load)
-        #         for r in res:
-        #             r['email'] = r[partner_email_field]
-        #         _logger.info('2222222222222222222222 %s', res)
-        #         return res
-        # res = super(ResPartner, self).read(list_fields, load)
-        # _logger.info('555555555555555555555555 %s', res)
-        if 'partner_email_field' in self._context:
-            partner_email_field = self._context.get('partner_email_field')
-            list_fields += [partner_email_field]
-            res = super(ResPartner, self).read(list_fields, load)
-            for r in res:
-                r['email'] = r[partner_email_field]
-            _logger.info('33333333333333333 %s', res)
-
-            return res
-        res = super(ResPartner, self).read(list_fields, load)
-        _logger.info('44444444444444444444 %s', res)
-        return res
+    #         return res
+    #     res = super(ResPartner, self).read(list_fields, load)
+    #     return res
