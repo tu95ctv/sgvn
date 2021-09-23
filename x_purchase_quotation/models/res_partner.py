faf28ec0b542cc3aca0436b6cb860e031c75b4ab
Hao Quach <49672733+haoquach1996@users.noreply.github.com>
2021-09-23 17:52:34 +0700

    Sgvn haoquach (#5)
    * complete send mail

--- x_purchase_quotation/models/res_partner.py
+++ x_purchase_quotation/models/res_partner.py
@@ -2,8 +2,25 @@
 
 from odoo import models, fields, api, _
 
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     email_quote_request = fields.Char("E-mail address for sending a quote request", copy=False)
     email_purchase = fields.Char("Purchase order sending email address", copy=False)
+
+    # def read(self, list_fields=None, load='_classic_read'):
+    #     if 'partner_email_field' in self._context:
+    #         partner_email_field = self._context.get('partner_email_field')
+    #         list_fields += [partner_email_field]
+    #         res = super(ResPartner, self).read(list_fields, load)
+    #         for r in res:
+    #             r['email'] = r[partner_email_field]
+
+    #         return res
+    #     res = super(ResPartner, self).read(list_fields, load)
+    #     return res
