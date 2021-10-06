27d46957dddbce1574b32fb9a598714bef1f4fc7
phuongphonui98 <phuongphonui98@gmail.com>
2021-10-06 16:33:32 +0700

    [REF] x_purchase_order: edit table field relation m2m

--- x_purchase_order/models/product_template.py
+++ x_purchase_order/models/product_template.py
@@ -13,6 +13,6 @@ class ProductTemplate(models.Model):
     po_qty_confirm = fields.Integer("Purchase Order quantity", default=0)
     po_amount_confirm = fields.Float("Purchase Order amount", default=0)
     po_uom_ids = fields.Many2many(
-        comodel_name="uom.uom", relation="purchase_uom_product_product_rel",
-        column1="product_id", column2="uom_id", string="Purchase Order Unit")
+        comodel_name="uom.uom", relation="purchase_uom_product_template_rel",
+        column1="product_tmpl_id", column2="uom_id", string="Purchase Order Unit")
     po_fixed_price = fields.Float("Purchase Fixed price", default=0)
