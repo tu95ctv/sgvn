25a1ae1aba381f3392abc490a6f1982854cd561d
phuongphonui98 <phuongphonui98@gmail.com>
2021-10-06 13:49:20 +0700

    [REF] purchase_report: update report

--- x_purchase_quotation/models/purchase.py
+++ x_purchase_quotation/models/purchase.py
@@ -25,35 +25,6 @@ class PurchaseOrder(models.Model):
                 route_id = self.env.ref('stock_dropshipping.route_drop_shipping', raise_if_not_found=False)
                 record.is_dropshipping = True if route_id in record.mapped('product_id').mapped('route_ids') else False
 
-    def _default_content(self):
-        return '''
-<div>
-<table style="border-collapse: collapse; width: 80%; height: 36px;">
-<tbody>
-<tr style="height: 18px; background-color: #f1c40f;">
-<td style="height: 18px; width: 56.5145%; text-align: center;" colspan="2"><span style="color: rgb(255, 255, 255); font-family: Calibri, sans-serif; font-size: 14px; line-height: 107%;">Direct shipping information</span></td>
-</tr>
-<tr style="height: 18px;">
-<td style="width: 42.2539%; height: 18px;">
-<p style="margin-bottom: 0cm; line-height: 1;"><span style="font-size: 14px;"><strong>Estimated price and estimated period for subcontracting work</strong></span></p>
-<p style="margin-bottom: 0cm; line-height: 1;"><span style="font-size: 14px;">Planned construction price</span></p>
-<p style="margin-bottom: 0cm; line-height: 1;"><span style="font-size: 14px;">Construction work less than 5 million yen</span></p>
-<p style="margin-bottom: 0cm; line-height: 1;"><span style="font-size: 14px;">Construction work of 5 million yen or more and less than 50 million yen</span></p>
-<p style="margin-bottom: 0cm; line-height: 1;"><span style="font-size: 14px;">Construction work of 50 million yen or more</span></p>
-</td>
-<td style="width: 14.2606%; height: 18px;">
-<p style="margin-bottom: 0cm; line-height: 1;"><font style="font-size: 14px;">&nbsp;</font></p>
-<p style="margin-bottom: 0cm; line-height: 1;"><span style="font-size: 14px;">Estimated period</span></p>
-<p style="margin-bottom: 0cm; line-height: 1;"><span style="font-size: 14px;">for 1 day or more</span></p>
-<p style="margin-bottom: 0cm; line-height: 1;"><span style="font-size: 14px;">for 10 days or more</span></p>
-<p style="margin-bottom: 0cm; line-height: 1;"><span style="font-size: 14px;">15 days or more during</span></p>
-</td>
-</tr>
-</tbody>
-</table>
-<p style="line-height: 1;"><font style="font-size: 14px;">&nbsp;</font></p>
-</div>
-        '''
 
     trans_classify_id = fields.Many2one('x.transaction.classification', "Transaction classification")
     type = fields.Selection([
@@ -67,7 +38,7 @@ class PurchaseOrder(models.Model):
     date_response = fields.Datetime("Response date", copy=False)
     date_issuance = fields.Date("Issuance date", copy=False, default=fields.Date.context_today)
     jurisdiction_id = fields.Many2one('crm.team', "Jurisdiction")
-    dest_address_infor = fields.Html("Direct shipping information", copy=False, default=_default_content, translate=html_translate, sanitize=False)
+    dest_address_infor = fields.Html("Direct shipping information", copy=False)
     # Displayed only when the transaction classification item of the slip is "Construction"
     show_construction = fields.Boolean("Show Construction", compute='_compute_show_construction')
     construction_name = fields.Char("Construction name")
