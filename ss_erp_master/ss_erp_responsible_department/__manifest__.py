1f2256d74088921eff8a7b6e88fcf847df7a5b5a
Trần Nhật Phương <phuongphonui98@gmail.com>
2021-10-08 14:39:32 +0700

    [IMP] ss_erp_responsible_department: add new module

---
+++ ss_erp_master/ss_erp_responsible_department/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "SS ERP Responsible Department",
+    "summary": """
+        SS ERP Responsible Department
+    """,
+    "version": "14.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://latido.vn",
+    "author": "LATIDO",
+    "depends": [
+    ],
+    "data": [
+        # SECURITY
+        "security/ir.model.access.csv",
+        # DATA
+        # VIEWS
+        "views/responsible_department_views.xml",
+        # REPORTS
+
+    ],
+    "application": False,
+    "installable": True,
+}
---
+++ ss_erp_master/ss_erp_responsible_department/models/responsible_department.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class ResponsibleDepartment(models.Model):
+    _name = 'responsible.department'
+
+    name = fields.Char(string='Name')
+    company_id = fields.Many2one(
+        'res.company', string='Company',required=True,
+        readonly=True, default=lambda self: self.env.company)
+    sequence = fields.Integer("Sequence")
+    active = fields.Boolean(default=True, help="If the active field is set to False, it will allow you to hide the payment terms without removing it.")
+    date_start = fields.Datetime(string="Valid start date", copy=False)
+    date_end = fields.Datetime(string="Expiration date", copy=False)
+    
+    
+    @api.constrains("date_start", "date_end")
+    def _check_dates(self):
+        """End date should not be before start date, if not filled
+
+        :raises ValidationError: When constraint is violated
+        """
+        for record in self:
+            if (
+                record.date_start
+                and record.date_end
+                and record.date_start > record.date_end
+            ):
+                raise ValidationError(
+                    _("The starting date cannot be after the ending date.")
+                )
