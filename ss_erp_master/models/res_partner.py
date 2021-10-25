5893d7112164766b39bd3e3f0f8af3459b0a8264
Trần Nhật Phương <phuongphonui98@gmail.com>
2021-10-25 10:20:52 +0700

    [REF] ss_erp_master: format module with autopep8

--- ss_erp_master/models/res_partner.py
+++ ss_erp_master/models/res_partner.py
@@ -3,11 +3,12 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     x_fax_number = fields.Char(
         string="Fax Number", size=20
     )
-    organization_id = fields.Many2one('ss_erp.organization', string='Organization')
-
+    organization_id = fields.Many2one(
+        'ss_erp.organization', string='Organization')
