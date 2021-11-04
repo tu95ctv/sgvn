360d25e196ecc0dca89d037229e5dad1bc7a477a
phuongphonui98 <phuongphonui98@gmail.com>
2021-11-04 13:43:32 +0700

    [REF] ss_erp: convert field has_performance_info -> has_perfomance_info

--- ss_erp/models/ss_erp_partner_construction.py
+++ ss_erp/models/ss_erp_partner_construction.py
@@ -10,8 +10,16 @@ class PartnerConstruction(models.Model):
     active = fields.Boolean('Active', default=True)
     sequence = fields.Integer('Sequence', default=10)
     license_figure = fields.Char(string='Type of permission')
-    license_flag_1 = fields.Char(string='Minister / Governor classification')
-    license_flag_2 = fields.Char(string='Specific / general classification')
+    license_flag_1 = fields.Selection([
+        ('minister', '大臣'),
+        ('governor', '知事'),
+        ('other', 'その他')
+        ],string='Minister / Governor classification', default='minister')
+    license_flag_2 = fields.Selection([
+        ('specific', '特定'),
+        ('normal', '一般'),
+        ('other', 'その他')
+        ],string='Specific / general classification', default='normal')
     license_number = fields.Char(string='Permission number')
     license_period = fields.Date(string='Permit date')
     partner_id = fields.Many2one('res.partner', string='Contact address')
