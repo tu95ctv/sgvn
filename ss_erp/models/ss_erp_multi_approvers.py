1c200ab8492deef322de240da308adbdbccf7cf2
Hao Quach <49672733+haoquach1996@users.noreply.github.com>
2021-11-04 13:25:13 +0700

    Sgvn haoquach (#24)
    * format Pep8 and command

--- ss_erp/models/ss_erp_multi_approvers.py
+++ ss_erp/models/ss_erp_multi_approvers.py
@@ -2,14 +2,17 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
+
 class MultiApprovers(models.Model):
     _name = 'ss_erp.multi.approvers'
     _description = 'Multi Approvers'
 
     x_company_id = fields.Many2one(
         'res.company', related='x_request_id.company_id', readonly=True, copy=False, store=True, index=True)
-    x_create_date = fields.Datetime('Created date', readonly=True, copy=True, store=True, index=False)
-    x_create_uid = fields.Many2one('res.users', string='Author', readonly=True, copy=True, store=True, index=False)
+    x_create_date = fields.Datetime('Created date', readonly=True,
+                                    copy=True, store=True, index=False)
+    x_create_uid = fields.Many2one('res.users', string='Author',
+                                   readonly=True, copy=True, store=True, index=False)
     x_display_name = fields.Char(string='display name', readonly=True)
     x_existing_request_user_ids = fields.Many2many(
         'res.users', 'existing_request_users_approvers_rel', 'user_id', 'approver_id', string='Existing Request User', readonly=True)
@@ -37,13 +40,16 @@ class MultiApprovers(models.Model):
     def _check_approver_group_minimum_approvers(self):
         for record in self:
             if len(record.x_approver_group_ids) < record.x_minimum_approvers:
-                raise UserError(_("You have to add at least %s approvers to confirm your request.", record.x_minimum_approvers))
+                raise UserError(
+                    _("You have to add at least %s approvers to confirm your request.", record.x_minimum_approvers))
 
     @api.depends('x_request_id.approver_ids', 'x_request_id.approver_ids.status')
     def _compute_x_user_status(self):
         for multi_approver in self:
+            # TODO: Fix here to get x_approval_seq and true value for x_user_status
             status_lst = multi_approver.x_request_id.mapped('approver_ids.status')
-            minimal_approver = multi_approver.x_minimum_approvers if len(status_lst) >= multi_approver.x_minimum_approvers else len(status_lst)
+            minimal_approver = multi_approver.x_minimum_approvers if len(
+                status_lst) >= multi_approver.x_minimum_approvers else len(status_lst)
             if status_lst:
                 if status_lst.count('cancel'):
                     status = 'cancel'
