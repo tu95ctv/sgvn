219f807aaa1646d9e67a70c0658ec469fba0856e
phuongphonui98 <phuongphonui98@gmail.com>
2021-11-24 08:43:10 +0700

    [REF] ss_erp: edit string for field

--- ss_erp/models/ss_erp_code_convert.py
+++ ss_erp/models/ss_erp_code_convert.py
@@ -8,6 +8,6 @@ class CodeConvert(models.Model):
 
     external_system = fields.Many2one('ss_erp.external.system.type', string='External system', required=True)
     convert_code_type = fields.Many2one('ss_erp.convert.code.type', string='Conversion code type', required=True)
-    external_code = fields.Char(string='Customer code (external)')
-    internal_code = fields.Char(string='Customer code (Odoo)')
+    external_code = fields.Char(string='External)')
+    internal_code = fields.Char(string='Code')
     priority_conversion = fields.Boolean(string='Priority conversion destination', required=True, default=False)
